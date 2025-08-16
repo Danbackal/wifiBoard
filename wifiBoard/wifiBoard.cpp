4c613b872ff08f788562551d6df953c0bd4e4bd7
Daniel Backal <danbackal@gmail.com>
2025-08-16 12:25:54 -0700

    basic IP wifi ping working

--- wifiBoard/wifiBoard.cpp
+++ wifiBoard/wifiBoard.cpp
@@ -2,6 +2,7 @@
 #include "pico/stdlib.h"
 #include "pico/cyw43_arch.h"
 
+// IP: 192.168.68.105
 
 
 
@@ -15,11 +16,21 @@ int main()
         return -1;
     }
 
+    cyw43_arch_enable_sta_mode();
+
+    printf("Connecting to WiFi ... /n");
+    if (cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000)) {
+        printf("Failed to connect... /n");
+        return -1;
+    } else {
+        printf("Connected");
+    }
+
     // Example to turn on the Pico W LED
     cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
 
     while (true) {
-        printf("Hello, world!\n");
-        sleep_ms(1000);
+        cyw43_arch_poll();
     }
+    cyw43_arch_deinit();
 }
